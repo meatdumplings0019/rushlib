f4d94ef9c80ff145d7a2bcd936fe204cb1bbbf84
ndrzy <dandan0019@outlook.com>
2025-09-11 20:15:32 +0800

    Initial commit

---
+++ rushlib/__init__.py
@@ -0,0 +1,1 @@
+import rushlib.color
---
+++ setup.py
@@ -0,0 +1,13 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name='rushlib',
+    version='0.1.0',
+    packages=find_packages(),
+    requires=[
+        'colorama'
+    ],
+    description='python lib',
+    author='ndrzy',
+    python_requires='>=3.10'
+)
